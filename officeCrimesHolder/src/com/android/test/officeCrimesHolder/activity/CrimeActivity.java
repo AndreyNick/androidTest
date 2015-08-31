887636ad87e4f5411cdc42f2a2d8ad5192e41738
AndreyNick <askneket@gmail.com>
2015-08-31 18:57:31 +0300

    officeCrimes: added intermediate part

--- officeCrimesHolder/src/com/android/test/officeCrimesHolder/activity/CrimeActivity.java
+++ officeCrimesHolder/src/com/android/test/officeCrimesHolder/activity/CrimeActivity.java
@@ -2,15 +2,29 @@ package com.android.test.officeCrimesHolder.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import com.android.test.officeCrimesHolder.R;
+import com.android.test.officeCrimesHolder.fragment.CrimeFragment;
 
 /**
  * Created by anni0913 on 31.08.2015.
  */
-public class CrimeActivity extends Activity {
+public class CrimeActivity extends FragmentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 }
--- officeCrimesHolder/src/com/android/test/officeCrimesHolder/fragment/CrimeFragment.java
+++ officeCrimesHolder/src/com/android/test/officeCrimesHolder/fragment/CrimeFragment.java
@@ -2,14 +2,47 @@ package com.android.test.officeCrimesHolder.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import com.android.test.officeCrimesHolder.R;
 import com.android.test.officeCrimesHolder.domain.Crime;
+
 
 
 public class CrimeFragment extends Fragment {
     private Crime crime;
+    private EditText textField;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         crime = new Crime();
+    }
+
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup parent, Bundle savedInstanceState){
+        View v = this.getLayoutInflater(savedInstanceState).inflate(R.layout.fragment_crime, parent, false);  //TODO it have to be inflater.inflate
+        textField = (EditText)v.findViewById(R.id.crime_title);
+        textField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                crime.setTitle(charSequence.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        return v;
+
     }
 }
